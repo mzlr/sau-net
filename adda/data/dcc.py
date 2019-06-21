5b7dee76dd74b10756347977edc7b459e8b9fb1b
mzlr <mzlr@users.noreply.github.com>
2019-06-20 21:49:48 -0700

    fix data path

--- adda/data/dcc.py
+++ adda/data/dcc.py
@@ -13,7 +13,7 @@ class DCC(object):
    Images are 296x296x3 images in the range [0, 255].
     """
 
-    data_path = '/projects/ashok/yueguo/multi-adda/DCC'
+    data_path = 'DCC'
 
     data_files = {
             'all': 'DCC',
